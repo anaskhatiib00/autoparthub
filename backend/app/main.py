2f2462f83a222787f4cdac0f809f752834c08ef9
Anas khatib <anas.khatiib@gmail.com>
2026-05-07 02:54:42 +0300

    Connect FastAPI to PostgreSQL database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.database import engine, Base
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
     return {"message": "AutoPartHub API Running"}
+
+@app.get("/db-test")
+def db_test():
+    return {"message": "Database connected successfully"}
